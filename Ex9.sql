96a28437726d7ee99c24e8fc3c746bd03b171f3c
Hưng Đinh <hungthkk123@gmail.com>
2026-06-15 16:48:45 +0700

    Ex6 ss11

--- Ex9.sql
+++ Ex9.sql
@@ -1,13 +1,42 @@
-create or replace procedure calculate_total_sales(
-    start_date DATE,
-    end_date DATE,
-    OUT total NUMERIC
-) language plpgsql
-as $$
+do $$
+    declare
+        A_id int :=1;
+        B_id int :=4;
+        money numeric(15,2) := 500;
+        A_balance numeric(15,2);
+        B_balance numeric(15,2);
     begin
-        select sum(amount) into total from sales
-        where sale_date between start_date and end_date
-    end;
-$$;
+        select balance into A_balance from accounts
+        where account_id=A_id
+        for update;
 
-CALL calculate_total_orders('2023-01-01', '2023-12-31', NULL);
+        select balance into B_balance from accounts
+        where account_id=B_id
+        for update;
+
+        if(A_balance is null) or (B_balance is null) then
+            raise exception 'Tài khoản gửi/nhận không tồn tại;';
+        end if;
+
+        if (select status from accounts where account_id=A_id ) != 'ACTIVE' or
+           (select status from accounts where account_id=B_id) != 'ACTIVE' then
+            raise exception 'Tài khoản gửi/nhận hiện tại không hoạt động';
+        end if;
+
+        if(A_balance < money) then
+            raise exception 'Tài khoản gửi hiện tại không đủ tiền';
+        end if;
+
+        update accounts
+        set balance=balance-money where account_id=A_id;
+
+        update accounts
+        set balance=balance+money where account_id=B_id;
+
+        insert into transactions(from_account, to_account, amount,status) VALUES
+        (A_id,B_id,money,'SUCCESS');
+    exception
+        when others then
+            raise notice 'Error: %', SQLERRM;
+        end;
+$$ language plpgsql;
